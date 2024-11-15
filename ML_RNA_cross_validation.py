9a11ca357fd9ac3e68a4f44424f24305dfe6b32a
Ilsazive-Ruo <ruoilsazive@gmail.com>
2024-11-15 21:51:18 +0800

    Update ML_RNA_cross_validation.py

--- ML_RNA_cross_validation.py
+++ ML_RNA_cross_validation.py
@@ -14,17 +14,17 @@ from sklearn.metrics import r2_score
 
 experiment_name = 'Multi-XGB'
 print(experiment_name)
-if os.path.exists('model7/' + experiment_name):
-    shutil.rmtree('model7/' + experiment_name)
-os.mkdir('model7/' + experiment_name)
+if os.path.exists('weight/' + experiment_name):
+    shutil.rmtree('weight/' + experiment_name)
+os.mkdir('weight/' + experiment_name)
 
 structure_props = {}
-SP_data = pd.read_csv('data7/structures_prop.csv')
+SP_data = pd.read_csv('data/structures_prop.csv')
 print(SP_data)
 for Num in SP_data['Num'].values:
     structure_props[Num] = SP_data.iloc[Num, 4:].values.tolist()
 print(structure_props)
-ds = pd.read_csv('data7/20241027.csv')
+ds = pd.read_csv('data/20241027.csv')
 
 x = []
 for i in ds.index:
@@ -34,14 +34,11 @@ for i in ds.index:
     x.append(temp)
 
 y = ds.iloc[:, 1:6].values
-# y = ds.iloc[:, 11].values
 y = np.array(y)
-print(y.shape)
 x = np.array(x)
-print(x.shape)
 k = 0
 
-skf = KFold(n_splits=5, shuffle=True, random_state=13) #13
+skf = KFold(n_splits=5, shuffle=True, random_state=13)
 r2 = {}
 FI = {}
 temp2 = []
@@ -49,15 +46,11 @@ score = 0
 for train_index, test_index in skf.split(y, x):
     k += 1
     print('k=', k)
-    # model = RandomForestRegressor(n_estimators=500, random_state=0)
-    # model = MultiTaskLasso(random_state=42)
-    # model = KNeighborsRegressor()
     model = xgb.XGBRegressor(objective='reg:squarederror', random_state=42)
-    # model = LinearRegression()
     x_train, x_test = x[train_index], x[test_index]
     y_train, y_test = y[train_index], y[test_index]
     model.fit(x_train, y_train)
-    joblib.dump(model, 'model7/' + experiment_name + '/' + experiment_name + '_' + str(k) + '.pkl')
+    joblib.dump(model, 'weight/' + experiment_name + '/' + experiment_name + '_' + str(k) + '.pkl')
     temp = []
     predict = model.predict(x_test)
     score += r2_score(y_test, predict)
@@ -74,5 +67,5 @@ print('AVE r2:', score/5)
 print('AVE LE:', sum(temp2)/5)
 df = pd.DataFrame(r2)
 df2 = pd.DataFrame(FI)
-df2.to_csv('model7/' + experiment_name + '/' + experiment_name + '_FI.csv', index=False)
-df.to_csv('model7/' + experiment_name + '/' + experiment_name + '_model.csv', index=False)
+df2.to_csv('weight/' + experiment_name + '/' + experiment_name + '_FI.csv', index=False)
+df.to_csv('weight/' + experiment_name + '/' + experiment_name + '_model.csv', index=False)
